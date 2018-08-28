eb335d3f49d9096efd5d789ca7aac600affb5229
DESKTOP-OC75RC4\lawre <lawrencemoshani@gmail.com>
2018-08-28 16:30:03 +0900

    no message

---
+++ Assets/Script/AshibaManager02.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AshibaManager02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider PlayerRightLeg)
+    {
+		 
+		Renderer rend001 = GetComponent<Renderer>();
+
+       	//Set the main Color of the Material to green
+        rend001.material.shader = Shader.Find("_Color");
+        rend001.material.SetColor("_Color", Color.blue);
+
+        //Find the Specular shader and change its Color to red
+        rend001.material.shader = Shader.Find("Specular");
+        rend001.material.SetColor("_SpecColor", Color.green);
+	 	
+		Debug.Log ("右足が当たった");
+	
+             
+    }
+}
