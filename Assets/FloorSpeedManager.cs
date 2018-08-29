f452b7114eee339993cdb38ce012037cf2c28ddd
DESKTOP-OC75RC4\lawre <lawrencemoshani@gmail.com>
2018-08-29 16:57:06 +0900

    no message

---
+++ Assets/FloorSpeedManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorSpeedManager : MonoBehaviour {
+
+	public ScriptableObject RailManager;
+	 public float speed = 0.01F;
+	 
+
+	// Use this for initialization
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+		
+    	
+	 	Debug.Log　("「Speed bump」に当たった");
+
+	
+             
+    }
+}
