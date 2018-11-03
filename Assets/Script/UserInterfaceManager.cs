7ed945787749655a2c2edd96fc411e2bde7e7cb3
DESKTOP-OC75RC4\lawre <lawrencemoshani@gmail.com>
2018-11-03 16:09:17 +0900

    [FIXED] Play_Test  Scene Commit002.

--- Assets/Script/UserInterfaceManager.cs
+++ Assets/Script/UserInterfaceManager.cs
@@ -8,18 +8,35 @@ public class UserInterfaceManager : MonoBehaviour {
 
 	//all the UI Image that I will use below
 
-	public Image StartImage;
+	//public Image StartImage;
 	public Image FinishImage;
-	public Text CountDownNumber; 
-
-
-	// Use this for initialization
-	void Start () {
-		
+	//public Text FinishText;
+	Text CountDownNumber; 
+	Color finishColour = new Color(255f, 255f, 255f,255f);
+	//Color finishTextColour = new Color(50f, 50f, 50f,255f);
+    //public Color normalColour = new Color(0f, 0f, 0f, 0.0f);
+	void Awake()
+	{
+		//FinishText = gameObject.FindGameObjectWithTag("FinishText");
 	}
+
+	void Start()
+	{
+		//FinishText.gameObject.SetActive(false);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+    {
+      //when the game ends show the stage clear scene 
+       FinishImage.color = finishColour;
+	   //FinishText.color = finishTextColour;
+	   //FinishText.gameObject.SetActive(true);
+	         
+    }
+
+    
+
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
